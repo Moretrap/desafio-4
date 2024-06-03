6fb6466be211ae9e0656e9ef1d92f18fc7d58cf1
Moretrap <traipecandela@gmail.com>
2024-06-02 23:00:15 -0300

    main

---
+++ desafio 4/main.js
@@ -0,0 +1,10 @@
+function sumarProductos (precioUnitario, cantidadDeseada) {
+    return precioUnitario * cantidadDeseada
+
+} 
+
+let precio = 300
+let cantidad = prompt ("¿Cuanto desea comprar?")
+
+alert(sumarProductos(precio, cantidad))
+console.log(sumarProductos(precio, cantidad))
